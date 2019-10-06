b905bc19ac509f5a5f1302ac2178b1a92d65cf85
Opender Singh <opender94@gmail.com>
2019-10-06 23:39:52 +1300

    tsconfig.test.json

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
 };
