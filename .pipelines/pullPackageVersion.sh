12552dcfc70df0d68544bb0f695b5afff56ebb58
Opender Singh <opender94@gmail.com>
2019-10-10 01:28:15 +1300

    Update

--- .pipelines/pullPackageVersion.sh
+++ .pipelines/pullPackageVersion.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 newVersion=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }'  | sed 's/[",]//g' | tr -d '[[:space:]]')
 currentVersion=$(npm show $(packageName) version)
 
