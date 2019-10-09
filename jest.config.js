559c8d19bc9ab037b8e94ddae92fac537c348132
Opender Singh <opender94@gmail.com>
2019-10-09 22:32:02 +1300

    Remove circleci and use azure pipelines

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['node_modules', 'dist'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 50, // aspire for 80
+      functions: 90, // aspire for 80
+      lines: 90, // aspire for 80
+      statements: 90, // aspire for 80
+    },
+  },
 };
