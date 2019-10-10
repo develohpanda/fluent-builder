cae674c9b7b757b81a069ca45d656620b2dd31b8
Opender Singh <opender94@gmail.com>
2019-10-10 22:27:25 +1300

    Update pipeline

--- .pipelines/pullPackageVersion.sh
+++ .pipelines/pullPackageVersion.sh
@@ -2,8 +2,8 @@
 newVersion=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }'  | sed 's/[",]//g' | tr -d '[[:space:]]')
 currentVersion=$(npm show $(packageName) version)
 
-echo "##vso[task.setvariable variable=version;]$newVersion"
-echo "##vso[task.setvariable variable=currentVersion;]$currentVersion"
+echo "##vso[task.setvariable variable=version]$newVersion"
+echo "##vso[task.setvariable variable=currentVersion]$currentVersion"
 
 echo "Package name: $(packageName)"
 echo "Current: $currentVersion"
